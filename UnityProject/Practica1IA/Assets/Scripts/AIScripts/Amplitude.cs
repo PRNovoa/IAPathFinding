f654448db33ce0552247a3bec8b6ebbb61ed2f83
Pablo Rodríguez <67236009+Toni-banamanoni@users.noreply.github.com>
2022-03-03 17:59:52 +0100

    +Started Amplitude Script
    Started the very basics of the first AI Algorithm.

--- UnityProject/Practica1IA/Assets/Scripts/AIScripts/Amplitude.cs
+++ UnityProject/Practica1IA/Assets/Scripts/AIScripts/Amplitude.cs
@@ -2,17 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Amplitude : MonoBehaviour
+namespace Assets.Scripts.DataStructures
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Node
     {
-        
+        public CellInfo currentNode { get; private set; }
+        public CellInfo previousNode { get; private set; }
+
+        public Locomotion.MoveDirection direction { get; private set; }
+
+        public Node() { }
+        public Node(CellInfo _currentNode, CellInfo _previousNode, Locomotion.MoveDirection _direction)
+        {
+            this.currentNode = _currentNode;
+            this.previousNode = _previousNode;
+
+            this.direction = _direction;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public class Amplitude : AbstractPathMind
     {
-        
+
+        /// <summary>
+        /// Called to check every cell until goal found
+        /// </summary>
+        public class GoalFinder
+        {
+
+        }
+
+        #region BaseClasses
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        #endregion
     }
 }
